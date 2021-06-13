a0260509ab916767deb6006c2a9cd7cd9561a1c8
Abadima <39747270+Abadima@users.noreply.github.com>
2021-06-13 11:45:57 -0400

    New Options & Bug Fixes
    This updated code features Small Image & 2nd Button

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ require('dotenv').config(); // This is for .env
 (async () => {
     client.on('ready', async () => {
         await client.setActivity({
-            buttons: [{ label: "Button Title Here", url: "Link For Button Here" }], // Buttons & URLs
+           buttons: [
+               { label: "Test", url: "URL_LINK"}, // (OPTIONAL) Button 1
+               { label: "Test2", url: "URL_LINK2"} // (OPTIONAL) Button 2
+            ], // Buttons & URLs ()
             details: "Description", // Description
-            largeImageKey: "rpc_icon", // Asset ID
-            largeImageText: "This text is shown when hovering over the largeImageKey in Discord." // When you hover your mouse over the image
+            largeImageKey: "lmk", // (OPTIONAL) Asset ID
+            largeImageText: "Testy Test.", // (OPTIONAL) When you hover your mouse over the image
+            smallImageKey: "starairlines", // (OPTIONAL) The small Image to the bottom right of your large
+            smallImageText: "Smol Testy Test." // (OPTIONAL) Small Image Text
         }).catch(err => console.log(err));
 
-        console.log("Discord Rich Presence has been enabled.");
+        console.log("Abadima's Rich Presence Active.");
     });
 
     await client.login({ clientId: process.env.applicationID }).catch(console.error);
